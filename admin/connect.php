3f1e24de197f0831d09e6325ebe399af2431ce82
benYassin126 <waelsalehhhh>
2019-09-15 10:29:23 +0300

    Add Dufelt Folder OF Admin From Earh Project

---
+++ admin/connect.php
@@ -0,0 +1,27 @@
+<?php
+
+    $dsn = "mysql:host=localhost;port=3307;dbname=earh"; //For Data Source Name We Will Use It As Parameter
+
+    $user ="root"; // For User We Will Use It As Parameter
+
+    $pass=""; // if We Have Pass We Will Use It As Parameter
+
+    $options = array (
+
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8" // For Ababic DataBase
+
+         ); // Colliction Of Optaions For Good Connect
+
+    try {
+
+            $db = new PDO ($dsn , $user , $pass , $options); // Start Connect To DataBase
+
+            $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); // Set Exception Mode
+
+
+    } catch (PDOException $e) {
+
+        echo  $e->getMessage(); // print Erorr Message
+    }
+
+ ?>
